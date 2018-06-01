7530fbdfb0a429c66c5aa9e810147c0523d86e9b
Leone-Hui <634811522@qq.com>
2018-06-01 18:28:18 +0800

    修改
    添加NSNumber数据类型

--- MacOSJsonFormatConversionTool/ViewController.m
+++ MacOSJsonFormatConversionTool/ViewController.m
@@ -29,7 +29,6 @@
     //OC-JSonDic
     NSDictionary* jsonDic = [[NSDictionary alloc] initWithContentsOfFile:filePath];
     NSLog(@"-jsonDic--%@",jsonDic);
-    NSLog(@"-jsonDic.allKeys--%@",jsonDic.allKeys);
     _sectionText.stringValue = [self stringWithJsonDic:jsonDic];
 }
 
@@ -134,43 +133,5 @@
     return path;
 }
 
-/*
- *NSString转NSDictionary
- */
--(NSDictionary *)dictionaryWithJsonString:(NSString *)jsonString {
-    if (jsonString == nil) {
-        return nil;
-    }
-    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
-    NSError *err;
-    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData
-                                                        options:NSJSONReadingMutableContainers
-                                                          error:&err];
-    if(err) {
-        NSLog(@"json解析失败：%@",err);
-        return nil;
-    }
-    return dic;
-    
-}
-
-#pragma mark - NSDictionary转NSString
-
-/*
- *NSDictionary转NSString
- */
--(NSString *)jsonStringWithDictionary:(NSDictionary *)object{
-    NSString *jsonString = nil;
-    NSError *error;
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:object
-                                                       options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
-                                                         error:&error];
-    if (! jsonData) {
-        NSLog(@"Got an error: %@", error);
-    } else {
-        jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    }
-    return jsonString;
-}
 
 @end
